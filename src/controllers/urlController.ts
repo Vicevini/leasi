9801d70920363c169bc86aa03e7fe5b65ae3bef1
Vinicius Oliveira <vicevini@live.com>
2024-08-14 11:01:57 -0300

    ajuste na controller de url

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -1,17 +1,25 @@
 import { Request, Response } from "express";
-import { nanoid } from "nanoid";
 import { AppDataSource } from "../data-source";
 import { URL } from "../models/URL";
-import { User } from "../models/User"; // Certifique-se de importar o modelo User se necessário
+import { User } from "../models/User";
 
 export const shortenUrl = async (req: Request, res: Response) => {
   const { original_url } = req.body;
+
+  // Use import() para módulos ES6
+  const { nanoid } = await import("nanoid");
   const short_url = nanoid();
 
   const shortenedUrl = new URL();
   shortenedUrl.original_url = original_url;
   shortenedUrl.short_url = short_url;
-  shortenedUrl.user = req.user ?? null; // Adicione a propriedade user
+
+  // Verifique se req.user existe e é um User completo
+  if (req.user) {
+    shortenedUrl.user = req.user as User; // Força o tipo User
+  } else {
+    shortenedUrl.user = null; // Ou null se não estiver autenticado
+  }
 
   try {
     await AppDataSource.getRepository(URL).save(shortenedUrl);
--- src/types.d.ts
+++ src/types.d.ts
@@ -3,7 +3,7 @@ import { User } from "../models/User";
 declare global {
   namespace Express {
     interface Request {
-      user?: User; // Torne o user opcional e use o tipo User
+      user?: User | null;
     }
   }
 }
