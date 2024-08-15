2f92814676e7d69977425a77fa5a71625708a78a
Vinicius Oliveira <vicevini@live.com>
2024-08-15 02:09:51 -0300

    wip: corrigindo erro em shortenUrl

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -6,26 +6,27 @@ import { User } from "../models/User";
 export const shortenUrl = async (req: Request, res: Response) => {
   const { original_url } = req.body;
 
-  const { nanoid } = await import("nanoid");
-  const short_url = nanoid();
-
-  const shortenedUrl = new URL();
-  shortenedUrl.original_url = original_url;
-  shortenedUrl.short_url = short_url;
-
-  if (req.user) {
-    shortenedUrl.user = req.user as User;
-  } else {
-    shortenedUrl.user = null;
-  }
-
   try {
+    const { nanoid } = await import("nanoid");
+    const short_url = nanoid();
+
+    const shortenedUrl = new URL();
+    shortenedUrl.original_url = original_url;
+    shortenedUrl.short_url = short_url;
+
+    if (req.user) {
+      shortenedUrl.user = req.user as User;
+    } else {
+      shortenedUrl.user = null;
+    }
+
     await AppDataSource.getRepository(URL).save(shortenedUrl);
     res.json({
       short_url: `${req.protocol}://${req.get("host")}/${short_url}`,
     });
   } catch (error) {
     res.status(500).json({ message: "Error shortening URL", error });
+    console.log(error);
   }
 };
 
